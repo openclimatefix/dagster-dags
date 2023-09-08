31511d0b1caf4baa6198d7760bff620256341e3e
devsjc <sol@openclimatefix.org>
2023-09-08 15:29:56 +0100

    Fix test

--- dags_tests/compile_test.py
+++ dags_tests/compile_test.py
@@ -3,5 +3,5 @@ from nwp import defs
 
 def test_compiles():
     job_names = [d.name for d in list(defs.get_all_job_defs())]
-    assert "get_ecmwf_data" in job_names
+    assert "nwp_consumer_docker_job" in job_names
     assert len(job_names) == 18
