9b5cc8fb2841e34259450d53abbe8d1c66874318
devsjc <47188100+devsjc@users.noreply.github.com>
2023-09-14 13:50:12 +0100

    Add partitioned schedule config to ECMWF job (#10)

--- nwp/assets/ecmwf/mars.py
+++ nwp/assets/ecmwf/mars.py
@@ -1,6 +1,5 @@
-from dagster_docker import execute_docker_container
 from dagster import Config, OpExecutionContext, op
-import datetime as dt
+from dagster_docker import execute_docker_container
 
 
 class NWPConsumerConfig(Config):
@@ -8,26 +7,22 @@ class NWPConsumerConfig(Config):
     date_to: str
     source: str
     docker_volumes: list[str]
+    env_vars: list[str]
 
 @op
 def nwp_consumer_docker_op(context: OpExecutionContext, config: NWPConsumerConfig):
     execute_docker_container(
-            context=context,
-            image="ghcr.io/openclimatefix/nwp-consumer",
-            command=[
-                "consume", f'--source={config.source}', 
-                f'--from={config.date_from}', 
-                f'--to={config.date_to}'
-            ],
-            env_vars=[
-                "ECMWF_API_KEY", "ECMWF_API_URL", "ECMWF_API_EMAIL",
-                "CEDA_FTP_USER", "CEDA_FTP_PASS",
-                "METOFFICE_ORDER_ID", "METOFFICE_CLIENT_ID", "METOFFICE_CLIENT_SECRET",
-                "AWS_S3_BUCKET", "AWS_REGION", "AWS_ACCESS_KEY", "AWS_ACCESS_SECRET",
-            ],
-            container_kwargs={
-                "volumes": docker_volumes
-            }
+        context=context,
+        image="ghcr.io/openclimatefix/nwp-consumer:latest",
+        command=[
+            "consume", f'--source={config.source}',
+            f'--from={config.date_from}',
+            f'--to={config.date_to}'
+        ],
+        env_vars=config.env_vars,
+        container_kwargs={
+            "volumes": config.docker_volumes
+        }
     )
 
     pass
