ef66117309c3e1bb8942a95564dd48a4cbdad826
devsjc <47188100+devsjc@users.noreply.github.com>
2023-09-15 17:19:19 +0100

    Change nwp-consumer job to call python package (#13)

--- nwp/assets/ecmwf/mars.py
+++ nwp/assets/ecmwf/mars.py
@@ -1,4 +1,5 @@
-from dagster import Config, OpExecutionContext, op
+import subprocess
+from dagster import Config, OpExecutionContext, op, graph
 from dagster_docker import execute_docker_container
 
 
@@ -7,6 +8,8 @@ class NWPConsumerConfig(Config):
     date_to: str
     source: str
     docker_volumes: list[str]
+    raw_dir: str
+    zarr_dir: str
     env_vars: list[str]
 
 @op
@@ -26,4 +29,33 @@ def nwp_consumer_docker_op(context: OpExecutionContext, config: NWPConsumerConfi
     )
 
     pass
+
+@op
+def nwp_consumer_download_op(context: OpExecutionContext, config: NWPConsumerConfig):
+    process = subprocess.run(
+        ["nwp-consumer", "download",
+         f'--source={config.source}', f'--from={config.date_from}', f'--to={config.date_to}',
+         f'--rdir={config.raw_dir}', f'--zdir={config.zarr_dir}'],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        text=True
+    )
+    code = process.returncode
+    print(process.stdout)
+    print(process.stderr)
+    return config
+
+@op
+def nwp_consumer_convert_op(context: OpExecutionContext, downloadedConfig: NWPConsumerConfig):
+    process = subprocess.run(
+        ["nwp-consumer", "convert",
+         f'--source={downloadedConfig.source}', f'--from={downloadedConfig.date_from}',
+         f'--to={downloadedConfig.date_to}',
+         f'--rdir={downloadedConfig.raw_dir}', f'--zdir={downloadedConfig.zarr_dir}'],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        text=True
+    )
+    print(process.stdout)
+    print(process.stderr)
 
