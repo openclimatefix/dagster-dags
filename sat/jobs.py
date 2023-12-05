a1b885df40af6cde1b2ac7c0a97bebca4486af7a
Jacob Bieker <jacob@bieker.tech>
2023-12-05 14:36:38 +0000

    Update Satellite jobs to use partitions

--- sat/jobs.py
+++ sat/jobs.py
@@ -1,22 +1,41 @@
 import pandas as pd
 from dagster import AssetSelection, define_asset_job, EnvVar
-
+import dagster
 from sat.assets.eumetsat.common import EumetsatConfig
 from sat.assets import download_eumetsat_iodc_data
+import datetime as dt
+from typing import Any
+import json
+import os
+
+jobs: list[dagster.JobDefinition] = []
+schedules: list[dagster.ScheduleDefinition] = []
 
 base_path = "/mnt/storage_c/IODC/"
 
-config = EumetsatConfig(api_key="",
-                        api_secret="",
-                        data_dir=base_path,
-                        start_date="2017-02-01",
-                        end_date=pd.Timestamp.utcnow().strftime('%Y-%m-%d'))
+# --- IODC jobs and schedules ----------------------------------------------
 
-asset_jobs = []
-asset_job = define_asset_job(f"download_iodc_raw_files", AssetSelection.assets(download_eumetsat_iodc_data),
-                             config={
-                                 'ops': {"download_eumetsat_iodc_data": {
-                                     "config": {"api_key": {'env': 'EUMETSAT_API_KEY'}, "api_secret": {'env': 'EUMETSAT_API_SECRET'}, "data_dir": config.data_dir,
-                                                "start_date": config.start_date, "end_date": config.end_date}}, }, })
+@dagster.daily_partitioned_config(start_date=dt.datetime(2017, 1, 1))
+def IODCDailyPartitionConfig(start: dt.datetime, _end: dt.datetime) -> dict[str, Any]:
+    # Do one day at a time
+    config = EumetsatConfig(
+        date=start.strftime("%Y-%m-%d"),
+        end_date=start.strftime("%Y-%m-%d"),
+        data_dir=base_path,
+        api_key=os.getenv("EUMETSAT_API_KEY"),
+        api_secret=os.getenv("EUMETSAT_API_SECRET"),
 
-asset_jobs.append(asset_job)
+    )
+    return {"ops": {"fetch_iodc_imagery_for_day": {"config": json.loads(config.json())}}}
+
+
+@dagster.job(
+    config=IODCDailyPartitionConfig,
+    tags={"source": "eumetsat", dagster.MAX_RUNTIME_SECONDS_TAG: 345600} # 4 days
+)
+def iodc_daily_archive() -> None:
+    """Download CAMS data for a given day."""
+    download_eumetsat_iodc_data()
+
+jobs.append(iodc_daily_archive)
+schedules.append(dagster.build_schedule_from_partitioned_job(iodc_daily_archive, hour_of_day=1))
