c83290f42001275d244c4078290b710d3a8a0e5f
Jacob Bieker <jacob@bieker.tech>
2023-12-05 14:49:44 +0000

    Add RSS and 0 Deg Service

--- sat/assets/__init__.py
+++ sat/assets/__init__.py
@@ -1,1 +1,3 @@
 from sat.assets.eumetsat.iodc import download_eumetsat_iodc_data
+from sat.assets.eumetsat.rss import download_eumetsat_rss_data
+from sat.assets.eumetsat.zero_deg import download_eumetsat_0_deg_data
--- sat/jobs.py
+++ sat/jobs.py
@@ -2,7 +2,7 @@ import pandas as pd
 from dagster import AssetSelection, define_asset_job, EnvVar
 import dagster
 from sat.assets.eumetsat.common import EumetsatConfig
-from sat.assets import download_eumetsat_iodc_data
+from sat.assets import download_eumetsat_iodc_data, download_eumetsat_0_deg_data, download_eumetsat_rss_data
 import datetime as dt
 from typing import Any
 import json
@@ -20,13 +20,13 @@ def IODCDailyPartitionConfig(start: dt.datetime, _end: dt.datetime) -> dict[str,
     # Do one day at a time
     config = EumetsatConfig(
         date=start.strftime("%Y-%m-%d"),
-        end_date=start.strftime("%Y-%m-%d"),
+        end_date=(start + pd.Timedelta("1d")).strftime("%Y-%m-%d"),
         data_dir=base_path,
         api_key=os.getenv("EUMETSAT_API_KEY"),
         api_secret=os.getenv("EUMETSAT_API_SECRET"),
 
     )
-    return {"ops": {"fetch_iodc_imagery_for_day": {"config": json.loads(config.json())}}}
+    return {"ops": {"download_eumetsat_iodc_data": {"config": json.loads(config.json())}}}
 
 
 @dagster.job(
@@ -34,8 +34,59 @@ def IODCDailyPartitionConfig(start: dt.datetime, _end: dt.datetime) -> dict[str,
     tags={"source": "eumetsat", dagster.MAX_RUNTIME_SECONDS_TAG: 345600} # 4 days
 )
 def iodc_daily_archive() -> None:
-    """Download CAMS data for a given day."""
+    """Download IODC data for a given day."""
     download_eumetsat_iodc_data()
 
 jobs.append(iodc_daily_archive)
-schedules.append(dagster.build_schedule_from_partitioned_job(iodc_daily_archive, hour_of_day=1))
+schedules.append(dagster.build_schedule_from_partitioned_job(iodc_daily_archive, hour_of_day=23))
+
+
+@dagster.daily_partitioned_config(start_date=dt.datetime(2008, 1, 1))
+def RSSDailyPartitionConfig(start: dt.datetime, _end: dt.datetime) -> dict[str, Any]:
+    # Do one day at a time
+    config = EumetsatConfig(
+        date=start.strftime("%Y-%m-%d"),
+        end_date=(start + pd.Timedelta("1d")).strftime("%Y-%m-%d"),
+        data_dir=base_path,
+        api_key=os.getenv("EUMETSAT_API_KEY"),
+        api_secret=os.getenv("EUMETSAT_API_SECRET"),
+
+    )
+    return {"ops": {"download_eumetsat_rss_data": {"config": json.loads(config.json())}}}
+
+
+@dagster.job(
+    config=RSSDailyPartitionConfig,
+    tags={"source": "eumetsat", dagster.MAX_RUNTIME_SECONDS_TAG: 345600} # 4 days
+)
+def rss_daily_archive() -> None:
+    """Download RSS data for a given day."""
+    download_eumetsat_rss_data()
+
+jobs.append(rss_daily_archive)
+schedules.append(dagster.build_schedule_from_partitioned_job(rss_daily_archive, hour_of_day=23))
+
+@dagster.daily_partitioned_config(start_date=dt.datetime(2008, 1, 1))
+def ZeroDegDailyPartitionConfig(start: dt.datetime, _end: dt.datetime) -> dict[str, Any]:
+    # Do one day at a time
+    config = EumetsatConfig(
+        date=start.strftime("%Y-%m-%d"),
+        end_date=(start + pd.Timedelta("1d")).strftime("%Y-%m-%d"),
+        data_dir=base_path,
+        api_key=os.getenv("EUMETSAT_API_KEY"),
+        api_secret=os.getenv("EUMETSAT_API_SECRET"),
+
+    )
+    return {"ops": {"download_eumetsat_0_deg_data": {"config": json.loads(config.json())}}}
+
+
+@dagster.job(
+    config=ZeroDegDailyPartitionConfig,
+    tags={"source": "eumetsat", dagster.MAX_RUNTIME_SECONDS_TAG: 345600} # 4 days
+)
+def zero_deg_daily_archive() -> None:
+    """Download RSS data for a given day."""
+    download_eumetsat_0_deg_data()
+
+jobs.append(zero_deg_daily_archive)
+schedules.append(dagster.build_schedule_from_partitioned_job(zero_deg_daily_archive, hour_of_day=23))
