1f2faf31371f1fac387c5e60dec7d46ec291046a
Jacob Bieker <jacob@bieker.tech>
2023-12-05 15:55:51 +0000

    Fix init

--- sat/__init__.py
+++ sat/__init__.py
@@ -2,9 +2,10 @@ from dagster import Definitions, load_assets_from_modules
 
 from sat import assets, jobs
 
+
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
-    jobs=jobs.asset_jobs,
+    schedules=jobs.schedules,
 )
