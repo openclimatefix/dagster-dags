cb2c14db314347c53c45d607e70dc8799af6da25
devsjc <47188100+devsjc@users.noreply.github.com>
2023-12-22 16:22:36 +0000

    Make ICON request and download in same step, reduce number of requests

--- local_archives/nwp/cams/_definitions_factory.py
+++ local_archives/nwp/cams/_definitions_factory.py
@@ -19,10 +19,16 @@ RAW_FOLDER = LOCATIONS_BY_ENVIRONMENT[env].RAW_FOLDER
 class VariableSelection:
     """Defines the variables to request from CAMS."""
 
-    slow: list[str] = dc.field(default_factory=list)
-    fast: list[str] = dc.field(default_factory=list)
+    # The slow variables are those that are only available from tape
+    # The dictionary maps the variable group name to a list of variables
+    # to pull within that groups' request. This can be one to [one].
+    slow: dict[str, list[str]] = dc.field(default_factory=dict)
+    # The fast variables are those that are available from disk
+    # The dictionary maps the variable group name to a list of variables
+    # to pull within that groups' request. This can be one to [one].
+    fast: dict[str, list[str]] = dc.field(default_factory=dict)
+    # The hours to pull
     hours: list[str] = dc.field(default_factory=list)
-
 
 @dc.dataclass
 class MakeDefinitionsOptions:
@@ -76,7 +82,6 @@ class CamsFileInfo:
 class MakeDefinitionsOutputs:
     """Outputs from the make_asset_definitions function."""
 
-    source_asset: dg.AssetsDefinition
     raw_asset: dg.AssetsDefinition
 
 
@@ -86,73 +91,141 @@ def make_definitions(
     """Generate the assets for a CAMS datset."""
 
     @dg.asset(
-        name="source_archive",
+        name="raw_archive",
         key_prefix=opts.key_prefix(),
         partitions_def=opts.partitions,
-        compute_kind="network_request",
+        compute_kind="network_request_download",
         op_tags={
-            "expected_runtime": "20min",
-            "MAX_RUNTIME_SECONDS_TAG": 20 * 60,
+            "expected_runtime": "5hrs",
+            "MAX_RUNTIME_SECONDS_TAG": 20 * 60 * 60,
         },
     )
-    def _cams_source_archive(context: dg.AssetExecutionContext) -> dg.Output[list[CamsFileInfo]]:
+    def _cams_raw_archive(context: dg.AssetExecutionContext) -> dg.Output[list[pathlib.Path]]:
         """Asset detailing all wanted remote files from CAMS."""
         execution_start = dt.datetime.now(tz=dt.UTC)
 
-        it = opts.partitions.parse_key(key=context.partition_key)
+        stored_files: list[pathlib.Path] = []
+        sizes: list[int] = []
+
         # Check if partition is targeting a time more than 30 days old
         # * CAMS data older than 30 days is only available from tape
         # * These variables are slower to collect
+        it = opts.partitions.parse_key(key=context.partition_key)
         use_slow: bool = False
         if (dt.datetime.now(tz=dt.UTC) - it) > dt.timedelta(days=30):
+            context.log.info(
+                f"Partition {context.partition_key} is targeting a time more than 30 days old. "
+                + "Pulling variables from tape, this may take a while.",
+            )
             use_slow = True
-
-        fis: list[CamsFileInfo] = []
 
         # First handle single level variables
         if opts.singlelevel_vars is not None:
-            for var in opts.singlelevel_vars.slow if use_slow else opts.singlelevel_vars.fast:
-                # Build the request
+            for name, varlist in (
+                opts.singlelevel_vars.slow if use_slow else opts.singlelevel_vars.fast
+            ).items():
+
+                # Create the target file path for the current set of vars
+                loc = "/".join(context.asset_key.path[:-1])
+                ext = ".grib" if opts.file_format == "grib" else ".nc"
+                dst = pathlib.Path(
+                    f"{RAW_FOLDER}/{loc}/{it.strftime(IT_FOLDER_FMTSTR)}/"
+                    + f"{it.strftime('%Y%m%d%H')}_{name}{ext}",
+                )
+                # If the file already exists, don't redownload it
+                if dst.exists():
+                    continue
+
+                dst.parent.mkdir(parents=True, exist_ok=True)
+                dst.touch()
+
+                # Othrwise, build the request
                 sl_var_request: dict[str, Any] = {
                     "date": it.strftime("%Y-%m-%d/%Y-%m-%d"),
                     "type": "forecast",
                     "format": opts.file_format,
-                    "variable": var,
+                    "variable": varlist,
                     "leadtime_hour": opts.singlelevel_vars.hours,
                     "time": it.strftime("%H:%M"),
                 }
                 if opts.area == "eu":
                     sl_var_request["model"] = "ensemble"
 
+                # Request the file and download it to the target
+                context.log.info("Reqesting file from CDS API", extra={
+                    "request": sl_var_request,
+                    "target": dst.as_posix(),
+                })
                 result = opts.client.retrieve(
                     name=opts.dataset_name(),
                     request=sl_var_request,
+                    target=dst.as_posix(),
                 )
-                fis.append(CamsFileInfo(**result.toJSON(), var=var, inittime=it))
+                stored_files.append(dst)
+                sizes.append(dst.stat().st_size)
+                context.log.info("File downloaded from CDS API", extra={
+                    "file": dst.as_posix(),
+                    "size": dst.stat().st_size,
+                })
+
+                # TODO: Split up multi-variables stored files into a single file per variable
+                # using grib_filter
 
         # Then handle multilevel variables
         if opts.multilevel_vars is not None:
-            for var in opts.multilevel_vars.slow if use_slow else opts.multilevel_vars.fast:
-                # Build the request
+            for name, varlist in (
+                opts.multilevel_vars.slow if use_slow else opts.multilevel_vars.fast
+            ).items():
+
+                # Create the target file path for the current set of vars
+                loc = "/".join(context.asset_key.path[:-1])
+                ext = ".grib" if opts.file_format == "grib" else ".nc"
+                dst = pathlib.Path(
+                    f"{RAW_FOLDER}/{loc}/{it.strftime(IT_FOLDER_FMTSTR)}/"
+                    + f"{it.strftime('%Y%m%d%H')}_{name}{ext}",
+                )
+
+                # If the file already exists, don't redownload it
+                if dst.exists():
+                    continue
+
+                dst.parent.mkdir(parents=True, exist_ok=True)
+                dst.touch()
+
+                # Othrwise, build the request
                 ml_var_request: dict[str, Any] = {
                     "date": it.strftime("%Y-%m-%d/%Y-%m-%d"),
                     "type": "forecast",
                     "format": opts.file_format,
-                    "variable": var,
+                    "variable": varlist,
                     "leadtime_hour": opts.multilevel_vars.hours,
                     "time": it.strftime("%H:%M"),
-                    "level": opts.multilevel_levels,
                 }
                 if opts.area == "eu":
+                    ml_var_request["level"] = opts.multilevel_levels
                     ml_var_request["model"] = "ensemble"
+                else:
+                    ml_var_request["pressure_level"] = opts.multilevel_levels
 
+                # Request the file and download it to the target
+                context.log.info("Reqesting file from CDS API", extra={
+                    "request": sl_var_request,
+                    "target": dst.as_posix(),
+                })
                 result = opts.client.retrieve(
                     name=opts.dataset_name(),
                     request=ml_var_request,
+                    target=dst.as_posix(),
                 )
-                fis.append(CamsFileInfo(**result.toJSON(), var=var, inittime=it))
+                stored_files.append(dst)
+                sizes.append(dst.stat().st_size)
+                context.log.info("File downloaded from CDS API", extra={
+                    "file": dst.as_posix(),
+                    "size": dst.stat().st_size,
+                })
 
-        if len(fis) == 0:
+
+        if len(stored_files) == 0:
             raise Exception(
                 "No remote files found for this partition key. See logs for more details.",
             )
@@ -160,78 +233,15 @@ def make_definitions(
         elapsed_time: dt.timedelta = dt.datetime.now(tz=dt.UTC) - execution_start
 
         return dg.Output(
-            fis,
+            stored_files,
             metadata={
                 "inittime": dg.MetadataValue.text(context.asset_partition_key_for_output()),
-                "num_files": dg.MetadataValue.int(len(fis)),
-                "partition_size": dg.MetadataValue.int(sum([fi.contentLength for fi in fis])),
+                "num_files": dg.MetadataValue.int(len(stored_files)),
+                "partition_size": dg.MetadataValue.int(sum(sizes)),
                 "elapsed_time_mins": dg.MetadataValue.float(elapsed_time / dt.timedelta(minutes=1)),
             },
         )
-
-    @dg.asset(
-        name="raw_archive",
-        key_prefix=opts.key_prefix(),
-        partitions_def=opts.partitions,
-        ins={"fis": dg.AssetIn(_cams_source_archive.key)},
-        compute_kind="download",
-        op_tags={
-            "expected_runtime": "20min",
-            "MAX_RUNTIME_SECONDS_TAG": 20 * 60,
-        },
-        metadata={
-            "archive_folder": dg.MetadataValue.text(f"{RAW_FOLDER}/{'/'.join(opts.key_prefix())}"),
-            "area": dg.MetadataValue.text(opts.area),
-        },
-    )
-    def _cams_raw_archive(
-        context: dg.AssetExecutionContext,
-        fis: list[CamsFileInfo],
-    ) -> dg.Output[list[pathlib.Path]]:
-        """Locally stored archive of raw data from CAMS."""
-        execution_start = dt.datetime.now(tz=dt.UTC)
-
-        stored_paths: list[pathlib.Path] = []
-        sizes: list[int] = []
-        # Iterate over the variables and their associated result objects from the cdsapi
-        for fi in fis:
-            # Store the file based on the asset key prefix and the init time of the file
-            loc = "/".join(context.asset_key.path[:-1])
-            ext = ".grib" if opts.file_format == "grib" else ".nc"
-            dst = pathlib.Path(
-                f"{RAW_FOLDER}/{loc}/{fi.inittime.strftime(IT_FOLDER_FMTSTR)}/" \
-                + f"{fi.inittime.strftime('%Y%m%d%H')}_{fi.var}{ext}",
-            )
-            # If the file already exists, don't redownload it
-            if dst.exists():
-                stored_paths.append(dst)
-                sizes.append(dst.stat().st_size)
-                continue
-
-            # Download the file using the CDS api. Target must be a list even though it
-            # is a single file - see the _download method on the Client class
-            # https://github.com/ecmwf/cdsapi/blob/master/cdsapi/api.py
-            dst.unlink(missing_ok=True)
-            stored_path = opts.client._download(fi.__dict__, target=[dst.as_posix()])
-            stored_paths.append(pathlib.Path(stored_path))
-
-        if len(stored_paths) == 0:
-            raise Exception("No raw files found for this partition key. See logs for more details.")
-
-        elapsed_time: dt.timedelta = dt.datetime.now(tz=dt.UTC) - execution_start
-
-        return dg.Output(
-            stored_paths,
-            metadata={
-                "inittime": dg.MetadataValue.text(context.asset_partition_key_for_output()),
-                "num_files": dg.MetadataValue.int(len(stored_paths)),
-                "file_locs": dg.MetadataValue.text(str([r.as_posix() for r in stored_paths])),
-                "elapsed_time_mins": dg.MetadataValue.float(elapsed_time / dt.timedelta(minutes=1)),
-            },
-        )
-
 
     return MakeDefinitionsOutputs(
-        source_asset=_cams_source_archive,
         raw_asset=_cams_raw_archive,
     )
