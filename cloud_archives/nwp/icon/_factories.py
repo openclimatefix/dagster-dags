15686792c9161ffddf7ae547ae0b52a305d44f6c
devsjc <47188100+devsjc@users.noreply.github.com>
2024-01-30 16:32:39 +0000

    Flesh out kbatch pipeline in icon factory

--- cloud_archives/nwp/icon/_factories.py
+++ cloud_archives/nwp/icon/_factories.py
@@ -1,9 +1,13 @@
 """Defines creation functions for icon assets."""
-
-
 import dataclasses as dc
+import datetime as dt
+import json
+import os
 
 import dagster as dg
+from kbatch._types import Job
+from kbatch import V1Job
+import kbatch._core as kbc
 
 
 @dc.dataclass
@@ -11,6 +15,7 @@ class MakeDefinitionsOptions:
     """Typesafe options for the _make_defintions function."""
 
     area: str
+    hf_repo_id: str
     partitions: dg.PartitionsDefinition
 
     def key_prefix(self) -> list[str]:
@@ -36,9 +41,37 @@ def _make_definitions(opts: MakeDefinitionsOptions) -> MakeDefinitionsOutputs:
             "MAX_RUNTIME_SECONDS_TAG": 20 * 60 * 60,
         },
     )
-    def zarr_asset() -> dg.AssetsDefinition:
-        """A zarr archive asset."""
-        pass
+    def zarr_asset(context: dg.AssetExecutionContext) -> dg.AssetsDefinition:
+        """A cloud-stored zarr archive of ICON data."""
+        execution_start = dt.datetime.now(tz=dt.UTC)
+
+        # Create a kbatch job to consume the data
+        job = Job(
+            name=f"icon-backfill-{context.partition_key}",
+            image="gchr.io/openclimatefix/nwp-consumer:0.2.0",
+            args=[
+                "consume",
+                "--source=icon",
+                "--sink=huggingface",
+                "--rdir=raw",
+                "--zdir=data",
+                f"--from={context.partition_key}",
+            ],
+            env={
+                "ICON_MODEL": opts.area,
+                "ICON_PARAMETER_GROUP": "full",
+                "HUGGINGFACE_TOKEN": os.environ["HUGGINGFACE_TOKEN"],
+                "HUGGINGFACE_REPO_ID": opts.hf_repo_id,
+            },
+        )
+
+        result = kbc._submit_job(
+            job,
+            model=V1Job,
+        )
+        job_name: str = result["metadata"]["name"]
+        pod_name: str = kbc.list_pods(job_name=job_name)["items"][0]["metadata"]["name"]
+        logs: list[str] = kbc._logs(pod_name=pod_name, stream=True, read_timeout=60 * 60)
 
     return MakeDefinitionsOutputs(
         zarr_asset=zarr_asset,
