949ebea23c909a863fef32fe2b5042f8902bea56
devsjc <47188100+devsjc@users.noreply.github.com>
2024-02-15 09:44:04 +0000

    Increase memory limit of kbatch job

--- cloud_archives/ops/kbatch.py
+++ cloud_archives/ops/kbatch.py
@@ -256,7 +256,7 @@ def submit_kbatch_job(context: dg.OpExecutionContext, job: Job) -> str:
         "resources": {
             "limits": {
                 "cpu": "8",
-                "memory": "59G",
+                "memory": "72G",
             },
             "requests": {
                 "cpu": "7.0",
