318c35006c69c2e8b52541da4fd2f7a005859163
devsjc <47188100+devsjc@users.noreply.github.com>
2024-05-31 15:52:24 +0100

    feat(gfs-etl): Combine into daily files

--- containers/gfs/download_combine_gfs.py
+++ containers/gfs/download_combine_gfs.py
@@ -5,6 +5,7 @@ import functools
 import logging
 import os
 import pathlib
+import shutil
 import sys
 import tempfile
 import uuid
@@ -12,6 +13,7 @@ from glob import glob
 from multiprocessing import Pool, cpu_count
 
 import cfgrib
+import dagster_pipes
 import requests
 import xarray as xr
 import zarr
@@ -66,7 +68,7 @@ def find_file_names(it: dt.datetime, config: Config) -> list[str]:
     return urls
 
 def convert_file(file: str, outfolder: str) -> str | None:
-    """Convert GRIB2 file to xarray dataset stored as a zipped zarr.
+    """Convert GRIB2 file to xarray dataset stored as a zarr.
 
     Storing the dile and passing that instead of an xaray dataset object saves memory.
     """
@@ -117,26 +119,39 @@ def convert_file(file: str, outfolder: str) -> str | None:
     )
     del surface_merged, heightAboveGround_merged, isobaricInhPa_merged
 
-    outfile = f"{outfolder}/{uuid.uuid4().hex}.zarr.zip"
-    with zarr.ZipStore(path=outfile, mode="w") as store:
+    outfile = f"{outfolder}/{uuid.uuid4().hex}.zarr"
+    with zarr.DirectoryStore(path=outfile) as store:
         total_ds.to_zarr(store, compute=True)
     del total_ds
     return outfile
 
-def _combine_same_it_datasets(dsp1: str, dsp2: str) -> str:
-    """Combine two datasets with the same init time."""
-    with zarr.ZipStore(dsp1, mode="r") as s1, zarr.ZipStore(dsp2, mode="r") as s2:
-        ds1 = xr.open_zarr(s1)
-        ds2 = xr.open_zarr(s2)
-        ds = xr.concat([ds1, ds2], dim="step")
-        del ds1, ds2
-    os.remove(dsp2)
-    with zarr.ZipStore(dsp2, mode="w") as s3:
-        ds.to_zarr(s3, compute=True)
-    return dsp2
+def _combine_datasets(dsp1: str, dsp2: str) -> str:
+    """Consolidate two datasets into a single one.
 
-def run(path: str, config: Config, date: dt.date) -> None:
-    """Download GFS data, combine, and save."""
+    Args:
+        dsp1: Path to the first dataset.
+        dsp2: Path to the second dataset.
+        step: The dimension to concatenate on.
+    """
+    cdsp = pathlib.Path(dsp1).parent / f"{uuid.uuid4().hex}.zarr"
+    with(
+        zarr.DirectoryStore(cdsp) as store,
+    ):
+        ds1 = xr.open_zarr(dsp1)
+        ds2 = xr.open_zarr(dsp2)
+        if ds1.coords["init_time"].values == ds2.coords["init_time"].values:
+            ds = xr.concat([ds1, ds2], dim="step")
+        elif ds1.coords["step"].values == ds2.coords["step"].values:
+            ds = xr.concat([ds1, ds2], dim="init_time")
+        else:
+            raise ValueError("No combination of datasets implemented for given dimensions.")
+        ds.to_zarr(store, compute=True)
+    shutil.rmtree(dsp1, ignore_errors=True)
+    shutil.rmtree(dsp2, ignore_errors=True)
+    return cdsp.as_posix()
+
+def run(path: str, config: Config, date: dt.date) -> str:
+    """Download GFS data, combine, and save for a single date."""
     # Dowload files first
     for hour in ["00", "06", "12", "18"]:
         if not pathlib.Path(f"{path}/{date:%Y%m%d}/{hour}/").exists():
@@ -175,6 +190,8 @@ def run(path: str, config: Config, date: dt.date) -> None:
 
     # Write files to zarr
     log.info("Converting files")
+
+    run_datasets: list[str] = []
     for hour in ["00", "06", "12", "18"]:
 
         dataset_paths: list[str] = []
@@ -183,22 +200,28 @@ def run(path: str, config: Config, date: dt.date) -> None:
                 ds_path = convert_file(file=file, outfolder=tmpdir)
                 if ds_path is not None:
                     dataset_paths.append(ds_path)
+            log.debug(f"Converted {len(dataset_paths)} files for {date}:{hour}")
 
-            hour_ds_path: str = functools.reduce(_combine_same_it_datasets, dataset_paths)
+            hour_ds_path: str = functools.reduce(_combine_datasets, dataset_paths)
+            log.debug(f"Combined {len(dataset_paths)} datasets for {date}:{hour}")
+            run_datasets.append(hour_ds_path)
 
-            ds = xr.open_zarr(zarr.ZipStore(hour_ds_path, mode="r"))
-            log.info(f"Dataset for {date}:{hour} created: {ds.to_dict(data=False)}")
-            encoding = {var: {"compressor": Blosc2("zstd", clevel=9)} for var in ds.data_vars}
-            encoding["time"] = {"units": "nanoseconds since 1970-01-01"}
-            with zarr.ZipStore(
-                f"{path}/{date:%Y%m%d}/{date:%Y%m%d}T{hour}.zarr.zip",
-                mode="w",
-            ) as store:
-                ds.to_zarr(
-                    store,
-                    encoding=encoding,
-                    compute=True,
-                )
+    log.info("Combining run datasets and applying compression")
+    day_ds_path: str = functools.reduce(_combine_datasets, run_datasets)
+    day_ds: xr.Dataset = xr.open_zarr(day_ds_path)
+    encoding = {var: {"compressor": Blosc2("zstd", clevel=9)} for var in day_ds.data_vars}
+    encoding["init_time"] = {"units": "nanoseconds since 1970-01-01"}
+    outpath = f"{path}/{date:%Y%m%d}/{date:%Y%m%d}.zarr.zip"
+    with zarr.ZipStore(path=outpath, mode="w") as store:
+        day_ds.to_zarr(
+            store,
+            encoding=encoding,
+            compute=True,
+        )
+
+    log.info(f"Saved dataset for {date:%Y%m%d} to {outpath}")
+    shutil.rmtree(day_ds_path, ignore_errors=True)
+    return outpath
 
 if __name__ == "__main__":
     prog_start = dt.datetime.now(tz=dt.UTC)
@@ -214,5 +237,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     log.info(f"{prog_start!s}: Running with args: {args}")
-    run(path=args.path, config=DEFAULT_CONFIG, date=args.date)
+    out = run(path=args.path, config=DEFAULT_CONFIG, date=args.date)
+
+    with dagster_pipes.open_dagster_pipes():
+        context = dagster_pipes.PipesContext.get()
+        context.report_asset_materialization(metadata={"path": out, "size": os.path.getsize(out)})
 
