07d41feb6a3a8e08de7ed8c84c42c22b83d9485c
devsjc <47188100+devsjc@users.noreply.github.com>
2024-09-11 08:38:14 +0100

    feat(local): Add MetOffice global asset (#125)
    * feat(local): Add MetOffice global asset
    
    * style(api): Update AutomizationPolicy -> AutomationCondition
    
    * chore(env): Bump dagster versions
    
    * fix(api): Use newer dagster API naming
    
    * fix(ceda_global): Add file
    
    * fix(ceda_global): Use constants for folders

--- local_archives/nwp/ceda/__init__.py
+++ local_archives/nwp/ceda/__init__.py
@@ -1,10 +1,15 @@
 import dagster as dg
 
-from . import ceda_uk
+from . import ceda_uk, ceda_global
 
 uk_assets = dg.load_assets_from_modules(
     modules=[ceda_uk],
     group_name="ceda_uk",
 )
 
-all_assets: list[dg.AssetsDefinition] = [*uk_assets]
+global_assets = dg.load_assets_from_modules(
+    modules=[ceda_global],
+    group_name="ceda_global",
+)
+
+all_assets: list[dg.AssetsDefinition] = [*uk_assets, *global_assets]
