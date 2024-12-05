e8cc229d231574a7885c7b0270a7f389861312c6
peterdudfield <peter.dudfield@hotmail.com>
2024-12-05 13:40:34 +0000

    update date

--- tests/cloud_archives/pv/test_passiv.py
+++ tests/cloud_archives/pv/test_passiv.py
@@ -4,5 +4,5 @@ from datetime import datetime, timezone
 
 
 def test_get_daily_passiv_data():
-    start_date = datetime(2022, 1, 1, tzinfo=timezone.utc)
+    start_date = datetime(2024, 12, 5, tzinfo=timezone.utc)
     get_monthly_passiv_data(start_date, upload_to_hf=False, overwrite=True)
