6462dc9d5e909580a3c91ce45c7abb6cdea274dd
devsjc <47188100+devsjc@users.noreply.github.com>
2025-01-20 09:50:39 +0000

    fix(test): Typing

--- tests/compile_test.py
+++ tests/compile_test.py
@@ -1,5 +1,7 @@
 import unittest
 
+import dagster as dg
+
 from local_archives import nwp_assets, sat_assets
 
 
@@ -7,6 +9,7 @@ class TestAssetKeyPrefixes(unittest.TestCase):
     def test_nwp_asset_key_prefixes(self) -> None:
         """Test asset keys for all nwp assets have the correct key structure."""
         for asset in [*nwp_assets, *sat_assets]:
-            # Ensure that the prefix is one of the expected flavours
-            self.assertIn( asset.key.path[0], ["nwp", "sat", "air"])
+            if isinstance(asset, dg.AssetsDefinition):
+                # Ensure that the prefix is one of the expected flavours
+                self.assertIn( asset.key.path[0], ["nwp", "sat", "air"])
 
