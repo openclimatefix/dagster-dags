27108eb45ea40daa324a640cd112c3332c684b1d
devsjc <47188100+devsjc@users.noreply.github.com>
2025-07-08 12:02:08 +0100

    fix(ceda): Correct env vars

--- src/dagster_dags/assets/nwp/ceda_mo_um_global.py
+++ src/dagster_dags/assets/nwp/ceda_mo_um_global.py
@@ -61,8 +61,8 @@ def ceda_mo_um_global_asset(
         image="ghcr.io/openclimatefix/nwp-consumer:1.1.28",
         command=["archive", "-y", str(it.year), "-m", str(it.month)],
         env={
-            "NWP_CONSUMER_MODEL_REPOSITORY": "ceda",
-            "NWP_CONSUMER_NOTIFICATION_REPOSITORY": "dagster-pipes",
+            "MODEL_REPOSITORY": "ceda",
+            "NOTIFICATION_REPOSITORY": "dagster-pipes",
             "CEDA_USER": os.environ["CEDA_USER"],
             "CEDA_PASS": os.environ["CEDA_PASS"],
             "CONCURRENCY": "true",
